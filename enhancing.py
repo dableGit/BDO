942aef00753d04681e7ee069d86383188488ac2b
dableGit <dable@web.de>
2022-12-12 16:23:00 +0100

    first commit

---
+++ enhancing.py
@@ -0,0 +1,165 @@
+import pandas as pd
+from prices import get_index, get_prices
+
+BS = 185000
+r_base = 12900
+reset = 100000
+Mem = 2800000
+CBSA = 2100000
+
+# Bheg prices
+PEN = 15000000000
+TET = 2740000000
+TRI = 1280000000
+DUO = 940000000
+PRI = 855000000
+
+# Grunil Helmet prices
+g_base = 530000
+g_six = 3060000
+g_ten = 14900000
+g_013 = 35100000
+g_PRI = 48000000
+g_DUO = 66000000
+g_TRI = 107000000
+g_TET = 525000000
+g_PEN = 930000000
+
+tax_rate = 0.84825
+
+df = pd.read_csv('Armor_FS.csv', sep=';')
+df = df / 100
+
+costs = []
+costs.append(0)
+
+# Calc FS Cost based on +14 Reblath enhancing
+repair = r_base / 2
+for fs in range(1, 121):
+    stake = BS + costs[-1]
+    c_succ = df['15'][fs-1]
+    c_fail = 1 - c_succ
+    cost = (stake + c_fail * repair + c_succ * reset)/c_fail
+    costs.append(cost)
+
+# for i,cost in enumerate(costs):
+#     print(i, int(cost/1000))
+
+
+def fs_costs(stake, res_succ, res_fail, succ_chances, fs_gain):
+    for fs, cost in enumerate(costs):
+        if fs >= len(costs)-fs_gain:
+            break
+        adj_stake = stake + cost
+        c_succ = succ_chances[fs]
+        c_fail = 1 - c_succ
+        new_cost = (adj_stake - c_succ * res_succ) / c_fail - res_fail
+        if new_cost < costs[fs+fs_gain]:
+            print(fs)
+            costs[fs+fs_gain] = new_cost
+
+
+# Reblath PRI -> DUO
+# fs_costs(CBSA + costs[25], costs[30], costs[25], df['DUO'], 3, r_base)
+# fs_costs(CBSA, 0, 0, df['DUO'], 3)
+# Reblath DUO -> TRI
+# fs_costs(CBSA + costs[30], costs[40], costs[25], df['TRI'], 4, r_base)
+# Reblath TRI -> TET
+# fs_costs(CBSA + costs[40], costs[50], costs[30], df['TET'], 5, r_base)
+
+
+# print('Grunil PRI -> DUO')
+# fs_costs(CBSA + g_PRI, g_DUO * 0.9, g_PRI - g_base, df['DUO'], 3)
+# print('Grunil DUO -> TRI')
+# fs_costs(CBSA + g_DUO, g_TRI * 0.9 ,g_PRI - g_base, df['TRI'], 4)
+# print('Grunil TRI -> TET')
+# fs_costs(CBSA + g_TRI, g_TET * 0.9, g_DUO - g_base, df['TET'], 5)
+
+s = pd.Series(costs)
+s.astype('int64').to_csv('test.csv')
+
+
+def boss_armor(stake, res_succ, res_fail, succ_chances, fs_gain):
+    gainz = []
+    for fs in range(121-fs_gain):
+        adj_stake = stake + costs[fs]
+        c_succ = succ_chances[fs]
+        c_fail = 1 - c_succ
+        adj_res_fail = costs[fs+fs_gain] + res_fail
+        gain = -adj_stake + c_fail * adj_res_fail + c_succ * res_succ
+        gainz.append(gain)
+    return gainz
+
+# boss_armor(CBSA + PRI, DUO, PRI - 10*Mem, df['DUO'], 3)
+# boss_armor(CBSA + DUO, TRI, PRI - 10*Mem, df['TRI'], 4)
+# boss_armor(CBSA + TRI, TET, DUO - 10*Mem, df['TET'], 5)
+
+
+class Accessory():
+
+    def __init__(self, name):
+
+        self.id = get_index(name)
+        self.name = name
+
+        df = get_prices(self.id)
+        self.prices = [int(df['Base Price'].values[i]) for i in range(6)]
+
+        self.base_chance = [0, 0.25, 0.1, 0.075, 0.025, 0.005]
+        self.soft_cap = [0, 18, 40, 44, 110, 330]
+
+    def get_chance(self, level, fs):
+        if fs <= self.soft_cap[level]:
+            return self.base_chance[level] + fs * self.base_chance[level] / 10
+        else:
+            return self.base_chance[level] + self.soft_cap[level] * self.base_chance[level] / 10 + (
+                fs - self.soft_cap[level]) * self.base_chance[level] / 50
+
+    def enhance(self, level):
+        best_fs = 0
+        best_profit = 0
+        stake = self.prices[0] + self.prices[level-1]
+        for fs, cost in enumerate(costs):
+            chance = self.get_chance(level, fs)
+            profit = self.prices[level] - cost - stake / chance
+            if profit > best_profit:
+                best_fs = fs
+                best_profit = profit
+        return (level, best_fs, int(best_profit/1000000))
+
+    def profits(self):
+        print(self.name)
+        print(self.enhance(1))
+        print(self.enhance(2))
+        print(self.enhance(3))
+
+
+items = [
+    "Laytenn's Power Stone",
+    'Ogre Ring',
+    'Tungrad Necklace',
+    'Revived Lunar Necklace',
+    'Deboreka Necklace',
+
+    'Forest Ronaros Ring',
+    'Ring of Crescent Guardian',
+    'Eye of the Ruins Ring',
+    'Ominous Ring',
+    'Tungrad Ring',
+
+    'Narc Ear Accessory',
+    'Ethereal Earring',
+    'Tungrad Earring',
+    'Black Distortion Earring',
+
+    "Orkinrad's Belt",
+    "Basilisk's Belt",
+    "Valtarra Eclipsed Belt",
+    "Tungrad Belt",
+    "Turo's Belt",
+    "Deboreka Belt",
+]
+
+for item in items:
+    acc = Accessory(item)
+    acc.profits()
---
+++ prices.py
@@ -0,0 +1,58 @@
+import requests
+import json
+import pandas as pd
+# from datetime import datetime
+
+
+def get_prices(idx):
+    url = "https://eu-trade.naeu.playblackdesert.com/Trademarket/GetWorldMarketSubList"
+    myjson = {
+        "keyType": 0,
+        "mainKey": idx
+    }
+
+    resp = convert_resp(requests.post(url, data=myjson))
+    headers = ['ItemID', 'Enhance min', 'Enhance max', 'Base Price', 'Current Stock',
+               'Total Trades', 'Price hardcap min', 'Price hardcap max', 'Last sale price', 'Last sale time']
+    df = pd.DataFrame(resp, columns=headers)
+    df['Last sale time'] = pd.to_datetime(df['Last sale time'], unit='s')
+    return df
+
+
+def get_bidding_info(idx, subkey):
+    url = "https://eu-trade.naeu.playblackdesert.com/Trademarket/GetBiddingInfoList"
+    myjson = {
+        "keyType": 0,
+        "mainKey": idx,
+        "subKey": subkey
+    }
+    headers = {
+        # 'Content-Type': 'application/json',
+        # 'User-Agent': 'BlackDesert'
+    }
+
+    x = requests.post(url, headers=headers, data=myjson)
+    print(x.text)
+    resp = convert_resp(x)
+    headers = ['Price', '# Sell Orders', '# Buy Orders']
+    df = pd.DataFrame(resp, columns=headers)
+    return df
+
+
+def convert_resp(resp):
+    mydata = json.loads(resp.text)
+    mydata = mydata['resultMsg']
+    new_list = []
+    for resp in mydata.split('|')[:-1]:
+        myresp = resp.split('-')
+        new_list.append(myresp)
+    return new_list
+
+
+# GET BDO id of an item from the item name
+def get_index(name):
+    prices = pd.read_csv('BDO Items.csv', sep=';')
+    cond = prices.Name.isin([name])
+    return prices[cond].Index.values[0]
+
+
